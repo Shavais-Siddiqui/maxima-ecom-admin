a778db744785fb4e4a65de46ff9319b1e9aefb15
Ayaz <ayazhussainbs@gmail.com>
2020-01-07 14:39:34 +0500

    BaseUrl added

--- src/app/layouts/admin-layout/Services/app.service.ts
+++ src/app/layouts/admin-layout/Services/app.service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppService {
 
-  baseUrl = 'http://localhost:3000/api/'
+  baseUrl = 'https://maximaecommerceserver.herokuapp.com/api/'
   constructor(private http: HttpClient) { }
 
   getProdcuts() {
